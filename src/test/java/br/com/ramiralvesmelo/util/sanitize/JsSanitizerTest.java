5db3bdf8ebe956c6867557b906d4d1f5ab4688c0
ramir.alves.melo <ramir.alves.melo@gmail.com>
2025-09-21 09:52:12 -0300

    ok

---
+++ src/test/java/br/com/ramiralvesmelo/util/sanitize/JsSanitizerTest.java
@@ -0,0 +1,29 @@
+package br.com.ramiralvesmelo.util.sanitize;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.ramiralvesmelo.util.sanitize.JsSanitizer;
+
+class JsSanitizerTest {
+
+    @Test
+    void shouldReturnNullLiteralWhenNull() {
+        assertEquals("null", JsSanitizer.clean(null));
+    }
+
+    @Test
+    void shouldQuoteAndEscapeBasics() {
+        assertEquals("\"abc\"", JsSanitizer.clean("abc"));
+        assertEquals("\"a\\\"b\"", JsSanitizer.clean("a\"b"));
+        assertEquals("\"a\\\\b\"", JsSanitizer.clean("a\\b"));
+    }
+
+    @Test
+    void shouldNormalizeNewlinesAndTabs() {
+        assertEquals("\"line1\\nline2\"", JsSanitizer.clean("line1\nline2"));
+        assertEquals("\"tab\\tstop\"", JsSanitizer.clean("tab\tstop"));
+        assertEquals("\"crlf\\nline\"", JsSanitizer.clean("crlf\r\nline"));
+    }
+}
