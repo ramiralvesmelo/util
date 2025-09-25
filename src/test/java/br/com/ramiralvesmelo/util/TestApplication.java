81ab805d0d82f1984db5879440608ef7187876a9
ramir.alves.melo <ramir.alves.melo@gmail.com>
2025-09-25 08:23:00 -0300

    ok

--- src/test/java/br/com/ramiralvesmelo/util/TestApplication.java
+++
@@ -1,9 +0,0 @@
-package br.com.ramiralvesmelo.util;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootApplication
-@ActiveProfiles("test")
-public class TestApplication {
-  // vazio: serve apenas como @SpringBootConfiguration para os testes
-}
